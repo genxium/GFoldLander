8d8cc806f2aad4aa9c9b27af2fc35182e5dc936a
yflu <genxium@hotmail>
2022-06-21 00:23:07 +0800

    Updated C++ sample codes to use BulletPhysics.

--- helloworldcxx/README.md
+++ helloworldcxx/README.md
@@ -2,6 +2,7 @@
 helloworldcxx> cd output 
 helloworldcxx/output> rm CMakeCache.txt 
 helloworldcxx/output> cmake ../ 
+# [WARNING] or "cmake ../ -G Xcode" to generate an Xcode project 
 
 # [WARNING] It's critical to specify "--config Release", see "CMakeLists.txt" for more information!
 helloworldcxx/output> cmake --build . --config Release 
--- helloworldcxx/main.cxx
+++ helloworldcxx/main.cxx
@@ -1,54 +1,87 @@
 #include "pandaFramework.h"
-#include "pandaSystem.h"
+#include "windowFramework.h"
+#include "nodePath.h"
+#include "clockObject.h"
 
+#include "asyncTask.h"
 #include "genericAsyncTask.h"
-#include "asyncTaskManager.h"
 
-// Global stuff
-PT(AsyncTaskManager) taskMgr = AsyncTaskManager::get_global_ptr();
-PT(ClockObject) globalClock = ClockObject::get_global_clock();
-NodePath camera;
+#include "bulletWorld.h"
+#include "bulletPlaneShape.h"
+#include "bulletBoxShape.h"
 
-// Task to move the camera
-AsyncTask::DoneStatus SpinCameraTask(GenericAsyncTask *task, void *data) {
-  double time = globalClock->get_real_time();
-  double angledegrees = time * 6.0;
-  double angleradians = angledegrees * (3.14 / 180.0);
-  camera.set_pos(20 * sin(angleradians), -20.0 * cos(angleradians), 3);
-  camera.set_hpr(angledegrees, 0, 0);
+BulletWorld *get_physics_world() {
+	// physics_world is supposed to be an global variable,
+	// but declaring global variables is not cool
+	// for good programmers lol, instead, should use static keyword.
+	static PT(BulletWorld) physics_world = new BulletWorld();
+	return physics_world.p();
+}
 
-  return AsyncTask::DS_cont;
+AsyncTask::DoneStatus update_scene(GenericAsyncTask* task, void* data) {
+	// Get dt (from Python example) and apply to do_physics(float, int, int);
+	ClockObject *co = ClockObject::get_global_clock();
+	get_physics_world()->do_physics(co->get_dt(), 10, 1.0 / 180.0);
+
+	return AsyncTask::DS_cont;
 }
 
 int main(int argc, char *argv[]) {
-  // Open a new window framework and set the title
-  PandaFramework framework;
-  framework.open_framework(argc, argv);
-  framework.set_window_title("My Panda3D Window");
+	// All variables.
+	PandaFramework framework;
+	WindowFramework *window;
+	NodePath camera;
+	LVecBase3 cameraOffset = LVecBase3(-10, 10, 100);
+	PT(AsyncTaskManager) task_mgr;
 
-  // Open the window
-  WindowFramework *window = framework.open_window();
-  camera = window->get_camera_group(); // Get the camera and store it
+	// Init everything :D
+	framework.open_framework(argc, argv);
+	framework.set_window_title("Bullet Physics");
 
-  // Load the environment model
-  NodePath scene = window->load_model(framework.get_models(), "models/environment");
-  scene.reparent_to(window->get_render());
-  scene.set_scale(0.25, 0.25, 0.25);
-  scene.set_pos(-8, 42, 0);
+	window = framework.open_window();
+	window->enable_keyboard();
+	window->setup_trackball();
 
-  // Load our panda
-  NodePath pandaActor = window->load_model(framework.get_models(), "models/panda-model");
-  pandaActor.set_scale(0.005);
-  pandaActor.reparent_to(window->get_render());
+	camera = window->get_camera_group();
+	// TODO: How to set camera pos and hpr based on that of the box?
+	task_mgr = AsyncTaskManager::get_global_ptr();
 
-  // Load the walk animation
-  window->load_model(pandaActor, "models/panda-walk4");
-  window->loop_animations(0); // bind models and animations
-                              //set animations to loop
+	// Make physics simulation.
+	// Static world stuff.
+	get_physics_world()->set_gravity(0, 0, -9.81f);
 
-  // Add our task do the main loop, then rest in peace.
-  taskMgr->add(new GenericAsyncTask("Spins the camera", &SpinCameraTask, nullptr));
-  framework.main_loop();
-  framework.close_framework();
-  return 0;
+	PT(BulletPlaneShape) floor_shape = new BulletPlaneShape(LVecBase3(0, 0, 1), 1);
+	PT(BulletRigidBodyNode) floor_rigid_node = new BulletRigidBodyNode("Ground");
+
+	floor_rigid_node->add_shape(floor_shape);
+
+	NodePath np_ground = window->get_render().attach_new_node(floor_rigid_node);
+	np_ground.set_pos(0, 0, -2);
+	get_physics_world()->attach(floor_rigid_node);
+
+	// Dynamic world stuff.
+	PT(BulletBoxShape) box_shape = new BulletBoxShape(LVecBase3(0.5, 0.5, 0.5));
+	PT(BulletRigidBodyNode) box_rigid_node = new BulletRigidBodyNode("Box");
+
+	box_rigid_node->set_mass(1.0f); // Gravity affects this rigid node.
+	box_rigid_node->add_shape(box_shape);
+
+	NodePath np_box = window->get_render().attach_new_node(box_rigid_node);
+	np_box.set_pos(0, 0, 5);
+	camera.set_pos(np_box.get_pos() + cameraOffset); // Seems like this line is not working as expected!	
+	get_physics_world()->attach(box_rigid_node);
+
+	NodePath np_box_model = window->load_model(framework.get_models(), "models/box");
+	np_box_model.set_pos(-0.5, -0.5, -0.5); // This is the positin within "np_box"
+	np_box.flatten_light();
+	np_box_model.reparent_to(np_box);
+
+	PT(GenericAsyncTask) task;
+	task = new GenericAsyncTask("Scene update", &update_scene, nullptr);
+	task_mgr->add(task);
+
+	framework.main_loop();
+	framework.close_framework();
+
+	return (0);
 }
